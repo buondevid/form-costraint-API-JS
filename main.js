1ede9b84751004b5b9c04b999d92ff9be212405a
buondevid <davidebriano@gmail.com>
2021-03-16 23:49:50 +0100

    further polish

--- main.js
+++ main.js
@@ -8,7 +8,42 @@ const passwordConfirm = form.querySelector('[id="password-confirm"]');
 email.addEventListener('change', (e) => {
 	email.setCustomValidity('');
 	if (email.checkValidity()) return;
-	console.log('ciao')
-	email.setCustomValidity('Please, enter a valid email type.');
+	if (email.validity.typeMismatch) email.setCustomValidity('Please, enter a valid email type.');
+	if (email.validity.valueMissing) email.setCustomValidity('Please, fill this field.');
 	email.reportValidity();
+});
+
+country.addEventListener('change', (e) => {
+	country.setCustomValidity('');
+	if (country.checkValidity()) return;
+	if (country.validity.typeMismatch) country.setCustomValidity('Please, enter a valid country name.');
+	if (country.validity.valueMissing) country.setCustomValidity('Please, fill this field.');
+	country.reportValidity();
+});
+
+zipcode.addEventListener('change', (e) => {
+	zipcode.setCustomValidity('');
+	if (zipcode.checkValidity()) return;
+	if (zipcode.validity.typeMismatch) country.setCustomValidity('Please, enter a valid zipcode number.');
+	if (zipcode.validity.rangeOverflow || zipcode.validity.rangeUnderflow) zipcode.setCustomValidity('Please, enter exactly 5 numbers for a valid zipcode.');
+	if (zipcode.validity.valueMissing) country.setCustomValidity('Please, fill this field.');
+	zipcode.reportValidity();
+});
+
+let passValue;
+
+password.addEventListener('change', (e) => {
+	password.setCustomValidity('');
+	passValue = password.value;
+	if (!/[0-9]/.test(passValue)) password.setCustomValidity('The password needs to have at least 1 number');
+	if (!/[a-z]/.test(passValue)) password.setCustomValidity('The password needs to have at least 1 lowercase letter');
+	if (!/[A-Z]/.test(passValue)) password.setCustomValidity('The password needs to have at least 1 uppercase letter');
+	if (password.validity.valueMissing) password.setCustomValidity('Please, choose a password.');
+	password.reportValidity();
+});
+
+passwordConfirm.addEventListener('change', (e) => {
+	const confirmValue = passwordConfirm.value;
+	if(confirmValue !== passValue) passwordConfirm.setCustomValidity('This doesn\'t match with the chosen password');
+	passwordConfirm.reportValidity();
 });
